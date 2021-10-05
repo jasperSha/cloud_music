6cb6953d2c787e96f00a2fbdedbb44fb6b7d601e
jasperSha <jaysha54@gmail.com>
2021-10-05 14:15:56 -0700

    added metadata scrape

--- scrape_youtube.py
+++ scrape_youtube.py
@@ -33,12 +33,28 @@ def pull_song(search_args, finished_songs, captions=False):
                 if info['id'] in finished_songs:
                     print('Already have this one, continuing...')
                     return
-        
+                
                 # duration in seconds, also check if exists (if premium can't extract info)
                 if info['duration'] <= 480 and not info['is_live']:
                     url = info['webpage_url']
                     print("Video title: ", info['title'], "URL: ", url)
                     ydl.download([url])
+        
+                # grabbing metadata here
+                artist = None # some videos do not have artists; this could be a reference for cleaning unwanted data
+                if 'artist' in info.keys():
+                    artist = info['artist']
+                tag = info['id']
+                title = info['title']
+                duration = info['duration']
+                print("Grabbing metadata of: ", title)
+                meta = {
+                        'id': tag, # youtube uses this in their url
+                        'artist': artist,
+                        'title': title,
+                        'duration': duration
+                        }
+                return meta
 
  
 if __name__ == '__main__':
@@ -61,10 +77,19 @@ if __name__ == '__main__':
     curr_count = 4955
     prev = curr_count
     
+    df = pd.read_csv("metadata.csv")
+    metadata = []
     for search in search_terms[curr_count:]:
         print("index: ", prev)
         prev += 1
-        pull_song(search, finished_songs)
+        song_meta_data = pull_song(search, finished_songs)
+
+        metadata.append(song_meta_data)
+        
+    # writing metadata to csv
+    df = pd.DataFrame.from_records(metadata)
+    df.to_csv("metadata.csv", encoding='utf-8', index=False, header=False, mode='a')
+    
 
     print("last index: ", prev)
     
