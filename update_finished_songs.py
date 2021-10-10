a7b139606f1f1c76f921d76ac1a1aac29353e5c7
jasperSha <jaysha54@gmail.com>
2021-10-10 15:00:40 -0700

    tweaks

--- update_finished_songs.py
+++ update_finished_songs.py
@@ -7,14 +7,21 @@ def update_finished_songs():
         runs through all flac audio file ids, compiles them, then updates the csv file of songs that have been downloaded
 
     '''
+    # get previous version of finished songs csv
+    prev = pd.read_csv('finished_songs.csv', header=None)
+    prev_done = prev[0].values.tolist()
 
     finished = []
     for fname in glob.glob('./spotify_yt_data/*.flac'):
         fname = fname.split('/')
         song_id = fname[-1][:-5]
-        finished.append(song_id)
+
+        # check if we've already recorded this song id as downloaded
+        if song_id not in prev_done:
+            finished.append(song_id)
     
     df = pd.DataFrame(finished)
+    # append new downloads to finished songs csv file
     df.to_csv("finished_songs.csv", encoding='utf-8',sep='\n', index=False,header=False,mode='a')
 
 if __name__ == '__main__':
