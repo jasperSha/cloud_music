5afd6daf227ba65e0f33cedc17fb568e03019737
jasperSha <jaysha54@gmail.com>
2021-10-17 17:02:53 -0700

    fixed metadata update issue

--- scrape_youtube.py
+++ scrape_youtube.py
@@ -39,6 +39,7 @@ def pull_song(search_args, finished_songs, captions=False):
                     url = info['webpage_url']
                     print("Video title: ", info['title'], "URL: ", url)
                     ydl.download([url])
+                    
         
                 # grabbing metadata here
                 artist = None # some videos do not have artists; this could be a reference for cleaning unwanted data
@@ -72,24 +73,29 @@ if __name__ == '__main__':
         
     df = pd.read_csv("finished_songs.csv", header=None)
     finished_songs = df[0].values.tolist()
-    
+
+    df = pd.read_csv("metadata.csv", header=0)
+    column_order = df.columns
+
     search_terms = songs
-    curr_count = 4955
+    curr_count = 20009
     prev = curr_count
     
     metadata = []
     for search in search_terms[curr_count:]:
         print("index: ", prev)
         prev += 1
+
+        song_meta_data = None
         song_meta_data = pull_song(search, finished_songs)
 
-        metadata.append(song_meta_data)
         
-    # writing metadata to csv
-    df = pd.DataFrame.from_records(metadata)
-    df.to_csv("metadata.csv", encoding='utf-8', index=False, header=False, mode='a')
-    
+        # writing metadata to csv
+        if song_meta_data is not None:
+            df = pd.DataFrame.from_records(song_meta_data, columns=column_order, index=[0])
+            df.to_csv("metadata.csv", encoding='utf-8', index=False, header=False, mode='a')
+        
 
-    print("last index: ", prev)
+        print("last index: ", prev)
     
 
